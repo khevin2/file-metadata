9d0d284eafb431ce8ca77ae317ef17dafcd805e9
cyusa <cyusa.khevin100@gmail.com>
2022-07-07 09:32:15 +0200

    Build 07July0932

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var fileupload = require("express-fileupload");
+var mime = require("mime-types");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
+app.use(fileupload());
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", function (req, res) {
+  var file;
 
+  if (!req.files) {
+    res.send("File was not found");
+    return;
+  }
 
+  file = req.files.upfile; // here is the field name of the form
+  const type = mime.contentType(file);
+  console.log(file);
+
+  res.send({ name: file.name, type: file.mimetype, size: file.size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
